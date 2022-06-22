751ed6b952e868e3482bbd090bdd0f83256ade38
hatimsue <hatimsue@gmail.com>
2022-06-22 17:43:46 -0500

    change provider.tf

--- provider.tf
+++ provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstate12528"
-    container_name       = "tfstate"
-    key                  = "test"
+    resource_group_name  = "xxxxx"
+    storage_account_name = "xxxxx"
+    container_name       = "xxxxx"
+    key                  = "xxxxx"
   }
 }
 provider "azurerm" {

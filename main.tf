38d0d6722c6b16d0841332a46c8791571b7fcd39
Jason Sheets <jason.sheets@sita.aero>
2023-07-13 10:54:04 -0400

    made significant changes

--- main.tf
+++ main.tf
@@ -1,26 +1,35 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.name
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "sita-sig-swa-pacfilehost" {
+  name     = var.resgroupname
   location = var.location
-}
-resource "azurerm_static_site" "example" {
-  name                = var.name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-}
-
-data "azurerm_storage_account_blob_container_sas" "name" {
-  connection_string = var.connection_string
-  container_name    = var.containername
-  start             = "2021-01-01"
-  expiry            = "2030-01-01"
-  permissions       = "rwdl"  
-}
-
-data "azurerm_storage_account" "name" { 
-  name = var.containername
-  resource_group_name = var.name
   
 }
+resource "azurerm_static_site" "sita-sig-staticsite-pacfilehost" {
+  name                = var.name
+  resource_group_name = azurerm_resource_group.sita-sig-swa-pacfilehost.name
+  location            = var.location
+}
+
+resource "null_resource" "frontend_files"{
+
+    depends_on = [data.azurerm_storage_account_blob_container_sas.container_name_sas, 
+                  azurerm_storage_account.pacfilestaticstorage]
+      provisioner "local-exec" {
+      command = <<EOF
+      
+      cd ../frontend      
+      azcopy login 
+      azcopy copy "./frontend" "${data.azurerm_storage_account_blob_container_sas.container_name_sas.connection_string}" --recursive=true
+      
+      EOF
+    }
+}
+
+
+
 
 # ######################################################################################################################
 # before creating azurerm_static_site_custom_domain resource you have to create a cname record in your domain provider.
@@ -36,24 +45,3 @@ data "azurerm_storage_account" "name" {
   validation_type = "cname-delegation"
 }
 */
-
-resource "null_resource" "frontend_files"{
-
-    depends_on = [data.azurerm_storage_account_blob_container_sas.website_blob_container_sas, 
-                  azurerm_storage_account.resume_static_storage]
-      provisioner "local-exec" {
-      interpreter = ["/bin/bash", "-c"]
-      command = <<EOF
-      cd ../frontend
-      
-      cd build
-
-      azcopy login 
-      azcopy copy "./frontend" "${azurerm_storage_account_blob_container_sas.website_blob_container_sas.sas}" --recursive=true
-      azcopy copy "./frontend" "${azurerm_storage_account.resume_static_storage.primary_blob_endpoint}" --recursive=true
-      EOF
-    }
-}
-
-
-

3f13a5a2df76a3e5c8b1746c71391f6b5cd1f78a
Jason Sheets <spyntek@outlook.com>
2023-08-30 21:22:05 -0400

    added back the azapi resource

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
 output "default_host_name" {
-  value       = azurerm_static_site.swapacfilehost.default_host_name
+  value       = azapi_resource.pacfilehostswa.name
   description = "The default host name of the Static Web App."
 }
+
+output "defaultHostname" {
+    value = jsondecode(azapi_resource.pacfilehostswa.output).properties.defaultHostname
+    description = "The json output of built static web app"
+   }
